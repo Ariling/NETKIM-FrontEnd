1a2e199a7ef012c568b691681e0a3a67a6c7c0df
조민규 <ariettys2@naver.com>
2024-08-08 00:30:07 +0900

    setting : test library setting, test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { InlineConfig, UserConfig, defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+  },
+} as VitestConfigExport);

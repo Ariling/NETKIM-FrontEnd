33dc5f1a1ca582f32d10f6e9142dbf9221320647
조민규 <ariettys2@naver.com>
2024-08-19 21:27:15 +0900

    setting : design & svg setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      mobile: { max: '640px' },
+      desktop: { min: '641px' },
+    },
+    extend: {
+      boxShadow: {
+        toggle: '0px 2px 12px 0px rgba(0,0,0,0.07)',
+        editShadow: '0px 12px 24px 0px rgba(13,16,45,0.1)',
+        editShadow2: '0px 25px 50px -12px rgba(13,16,45,0.1)',
+      },
+      colors: {
+        // 토글 셀렉
+        'toggle-select': '#EAECEE',
+        // 메인 컬러 조합
+        'peach-header': 'rgba(255,190,152,0.37)',
+        'peach': '#FFB2A5',
+        'peach-light': 'rgba(255,178,165,0.7)',
+        'peach-thick': '#F97272',
+        'peach-semiThick': 'rgba(249,114,114,0.68)',
+        // 글 쓰기 컬러
+        'basic': '#F7F3ED',
+      },
+    },
   },
   plugins: [],
 };

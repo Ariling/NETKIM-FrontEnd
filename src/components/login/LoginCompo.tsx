569bbeb1fdc6cc89f439ac5963da3ca01d7db6e8
조민규 <ariettys2@naver.com>
2024-08-28 00:23:27 +0900

    ✨ feat : 로그인 페이지 구현

--- src/components/login/LoginCompo.tsx
+++ src/components/login/LoginCompo.tsx
@@ -1,9 +1,25 @@
 import LoginVector from '@assets/img/LoginVector.webp';
-import GoogleBtn from '@assets/img/googleStart.png';
-import KakaoBtn from '@assets/img/kakaoStart.png';
+import Btn from '../common/Btn';
+import { useNavigate } from 'react-router-dom';
+import { useReducer, useState } from 'react';
+import { inputReducer } from '@/store/editReducer';
+import { regExpEmail } from '@/constants/regExp';
 
 const LoginCompo = () => {
   const welcome: String = 'Welcome!NETKIM';
+  const navigate = useNavigate();
+  const [email, emailMethod] = useReducer(inputReducer, '');
+  const [pw, pwMethod] = useReducer(inputReducer, '');
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [isValidPw, setIsValidPw] = useState(false);
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    emailMethod({ type: 'CHANGE', payload: e.target.value });
+    setIsValidEmail(regExpEmail.test(email));
+  };
+  const onChangePw = (e: React.ChangeEvent<HTMLInputElement>) => {
+    pwMethod({ type: 'CHANGE', payload: e.target.value });
+    setIsValidPw(pw.length >= 7);
+  };
   return (
     <div className="flex items-center gap-[65px]">
       <div className="justify-between w-1/2">
@@ -13,29 +29,46 @@ const LoginCompo = () => {
         <pre className="text-[43px] font-black leading-[52.5px] whitespace-pre-line">{welcome}</pre>
         <div className="text-[25px] font-semibold leading-[52.5px] mb-[70px]">어쩌구 저쩌구</div>
         <div>
-          <img
-            src={KakaoBtn}
-            alt="카카오로 로그인하기"
-            style={{
-              marginBottom: '41px',
-              cursor: 'pointer',
-            }}
-            fetchPriority="high"
-            onClick={() => alert('카카오')}
-          />
-          <img
-            src={GoogleBtn}
-            alt="구글로 로그인하기"
-            style={{
-              marginBottom: '58px',
-              cursor: 'pointer',
-            }}
-            fetchPriority="high"
-            onClick={() => alert('구글')}
-          />
+          <div className="flex flex-col items-start mb-4">
+            <div className="text-lg font-semibold">이메일</div>
+            <input
+              className="text-left h-10 w-full"
+              placeholder="ex) abc@www.com"
+              value={email}
+              onChange={onChangeEmail}
+            />
+          </div>
+          <div className="flex flex-col items-start">
+            <div className="text-lg font-semibold">비밀번호</div>
+            <input
+              className="text-left h-10 w-full"
+              placeholder="8자리 이상 입력"
+              type="password"
+              value={pw}
+              onChange={onChangePw}
+            />
+          </div>
           <div>
-            소셜 계정을 통해 바로 이용이 가능하며
-            <br />첫 로그인시 이용약관 및 개인정보처리방침 동의로 간주됩니다.
+            <Btn
+              name="로그인"
+              onClick={() => {
+                if (isValidEmail && isValidPw) {
+                  alert('로그인 API');
+                  pwMethod({ type: 'RESET' });
+                  emailMethod({ type: 'RESET' });
+                  setIsValidEmail(false);
+                  setIsValidPw(false);
+                } else {
+                  alert('이메일 형식을 지키거나 비밀번호 8자리를 입력하세요');
+                }
+              }}
+              className="edit_btn bg-peach-semiThick mt-5 mr-4"
+            />
+            <Btn
+              name="회원가입"
+              onClick={() => navigate('/signup')}
+              className="edit_btn bg-peach mt-5"
+            />
           </div>
         </div>
       </div>
--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -2,9 +2,9 @@ import LoginCompo from '@/components/login/LoginCompo';
 
 const LoginPage = () => {
   return (
-    <div className="bg-basic w-full h-screen">
-      <div className=" inline-block w-3/4 my-20 mx-auto">
-        <div className="bg-white rounded-lg ">
+    <div className="bg-basic w-full min-h-screen flex items-center justify-center py-10">
+      <div className="w-3/4 min-w-[1280px] max-w-3xl">
+        <div className="bg-white rounded-lg">
           <LoginCompo />
         </div>
       </div>
